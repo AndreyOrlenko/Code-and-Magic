5bfe3036198fb80da11d0e0ab838a3eca05adcc6
AndreyOrlenko <andriiorlenkoo@gmail.com>
2018-06-25 20:58:45 +0300

    Реализовал drag and drop для popup-а и для звездочек в инвентаре волшебника, выполнен рефакторинг уже написанного кода

--- src/js/setup.js
+++ src/js/setup.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//Добавление похожих волшебников
+
 var setup = document.querySelector('.setup');
 var setupSimilar = document.querySelector('.setup-similar');
 var setupSimilarList = document.querySelector('.setup-similar-list');
@@ -74,13 +76,20 @@ function RandomSequenceConcatNameAndSurname(arrNames, arrSurnames) {
   }
 }
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
-setupSimilarList.appendChild(fragment);
-setupSimilar.classList.remove('hidden');
 
+
+(function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  setupSimilarList.appendChild(fragment);
+  setupSimilar.classList.remove('hidden');
+})();
+
+
+
+//Открытие/закрытие попапа
 
 var buttonOpen = document.querySelector('.setup-open');
 var buttonClose = setup.querySelector('.setup-close');
@@ -92,8 +101,6 @@ var counterCoat = 1;
 var counterEye = 1;
 var counterFireball = 1;
 
-
-
 var OpenSetupPopup = function () {
   setup.classList.remove('hidden');
   document.addEventListener('keydown', onPopupKeydownEsc);
@@ -101,6 +108,7 @@ var OpenSetupPopup = function () {
 var CloseSetupPopup = function () {
   setup.classList.add('hidden');
   document.removeEventListener('keydown', onPopupKeydownEsc);
+  setup.setAttribute('style', '');
 };
 var onPopupKeydownEsc = function (evt) {
   if (document.activeElement.className !== 'setup-user-name') {
@@ -109,31 +117,6 @@ var onPopupKeydownEsc = function (evt) {
     }
   }
 };
-
-var onSetupWizardCoatClick = function() {
-  setupWizardCoat.style.fill = coatColors[counterCoat];
-  counterCoat++;
-  if (counterCoat === coatColors.length) {
-    counterCoat = 0;
-  }
-};
-
-var onSetupWizardEyeClick = function() {
-  setupWizardEye.style.fill = eyesColors[counterEye];
-  counterEye++;
-  if (counterEye === eyesColors.length) {
-    counterEye = 0;
-  }
-};
-
-var onSetupWizardFireballClick = function() {
-  setupWizardFireball.style.backgroundColor = fireballsColors[counterFireball];
-  counterFireball++;
-  if (counterFireball === fireballsColors.length) {
-    counterFireball = 0;
-  }
-};
-
 
 buttonOpen.addEventListener('click', function () {
   OpenSetupPopup();
@@ -155,6 +138,31 @@ buttonClose.addEventListener('keydown', function (evt) {
   }
 });
 
+//Изменение цвета мантии, глаз, фаербола
+
+var onSetupWizardCoatClick = function () {
+  setupWizardCoat.style.fill = coatColors[counterCoat];
+  counterCoat++;
+  if (counterCoat === coatColors.length) {
+    counterCoat = 0;
+  }
+};
+
+var onSetupWizardEyeClick = function () {
+  setupWizardEye.style.fill = eyesColors[counterEye];
+  counterEye++;
+  if (counterEye === eyesColors.length) {
+    counterEye = 0;
+  }
+};
+
+var onSetupWizardFireballClick = function () {
+  setupWizardFireball.style.backgroundColor = fireballsColors[counterFireball];
+  counterFireball++;
+  if (counterFireball === fireballsColors.length) {
+    counterFireball = 0;
+  }
+};
 
 setupWizardCoat.addEventListener('click', onSetupWizardCoatClick);
 
@@ -162,5 +170,53 @@ setupWizardEye.addEventListener('click', onSetupWizardEyeClick);
 
 setupWizardFireball.addEventListener('click', onSetupWizardFireballClick);
 
+//Перетаскивание артефактов
+
+var setupShop = setup.querySelector('.setup-artifacts-shop');
+var setupArtifacts = setup.querySelector('.setup-artifacts');
+var draggedItem = null;
+
+setupShop.addEventListener('dragstart', function (evt) {
+  if (evt.target.tagName.toLowerCase() === 'img') {
+    draggedItem = evt.target;
+    evt.dataTransfer.setData('text/plain', evt.target.alt);
+    setupArtifacts.style.outline = '2px dashed red';
+  }
+});
+
+setupShop.addEventListener('dragend', function () {
+  setupArtifacts.style.outline = '';
+});
+
+setupArtifacts.addEventListener('dragover', function (evt) {
+  evt.preventDefault();
+  return false;
+
+});
+
+setupArtifacts.addEventListener('drop', function (evt) {
+
+  if (evt.target.children.length === 0) {
+    if (!(evt.target.tagName.toLowerCase() === 'img')) {
+      evt.target.style.backgroundColor = '';
+      evt.target.appendChild(draggedItem.cloneNode());
+      setupArtifacts.style.outline = '';
+      evt.preventDefault();
+    }
+  }
+});
+
+setupArtifacts.addEventListener('dragenter', function (evt) {
+  if (evt.target.children.length === 0) {
+    if (!(evt.target.tagName.toLowerCase() === 'img')) {
+      evt.target.style.backgroundColor = 'yellow';
+      evt.preventDefault();
+    }
+  }
+});
+setupArtifacts.addEventListener('dragleave', function (evt) {
+  evt.target.style.backgroundColor = '';
+  evt.preventDefault();
+});
 
 
